1abe968302939cf40ff9184e6487b685fd095221
alejandro.ariza@ <alejandro.ariza@macroproyectos.com>
2026-05-16 03:34:35 -0500

    Configuracion inicial del modelo Usuario y migraciones

--- backend/apps/usuarios/models.py
+++ backend/apps/usuarios/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Datos para la db
+ROLES = [
+    ("admin","Administrador"),
+    ("campesino","Campesino"),
+    ("comprador","Comprador"),
+]
 
 # Create your models here.
+class Usuario(AbstractUser):
+    documento = models.CharField(max_length=20)
+    celular = models.CharField(max_length=20)
+    rol = models.CharField(
+        max_length=20,
+        choices=ROLES
+    )
